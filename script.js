982a2566eebe550f50dbcd2a634a86a0041e15f1
vincentThapelo <vthapelo@gmail.com>
2022-03-17 12:37:41 -0700

    commiting files

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿
+
+var weight = document.getElementById('weight').value;
+var pricePerKilo = document.getElementById('price').value;
+
+    function paySalary()
+    {
+        var weight = document.getElementById('weight').value;
+        var pricePerKilo = document.getElementById('price').value;
+        var weight1 = weight;
+        var price = pricePerKilo;
+
+        var totalPay = weight1 * price;
+
+        document.getElementById("amount").value = totalPay;
+    
+    
+    }
+
+    
